ba2aa9309bdad62d49f7088313d97a1c6e601230
gksgustjq77 <gksgustjq77@gmail.com>
2025-09-19 18:29:00 +0900

    미디어쿼리 추가 (모바일 패딩 제거)

--- src/components/PostComments.tsx
+++ src/components/PostComments.tsx
@@ -2,26 +2,25 @@
 import { formatDistanceToNow, parseISO } from "date-fns";
 import type { Comment } from "../types/CommentType";
 import { ko } from "date-fns/locale";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
-interface PostCommenstProps {
+interface PostCommentsProps {
   comments: Comment[];
   isOpen: boolean;
 }
 
-const PostComments: React.FC<PostCommenstProps> = ({ comments, isOpen }) => {
+const PostComments: React.FC<PostCommentsProps> = ({ comments, isOpen }) => {
   if (!isOpen) return null;
+
   return (
-    <div
-      className={`flex flex-col overflow-hidden transition-all duration-300 ${
-        isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
-      }`}
-    >
+    <div className="flex flex-col">
       {comments.map((comment, idx) => (
         <div
           key={idx}
-          className="flex items-start items-center gap-4 border-t border-gray-200 py-2"
+          className="flex items-start gap-4 border-t border-gray-200 py-2"
         >
-          <img
+          <LazyLoadImage
             src={comment.author?.profileImage}
             alt={comment.author?.nickname}
             className="h-full w-auto rounded-full object-cover"
@@ -32,7 +31,6 @@ const PostComments: React.FC<PostCommenstProps> = ({ comments, isOpen }) => {
             </p>
             <p className="text-sm text-gray-700">{comment.content}</p>
             <p className="text-xs text-gray-400">
-              {" "}
               {formatDistanceToNow(parseISO(comment.createdAt), {
                 addSuffix: true,
                 locale: ko,
